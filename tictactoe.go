5e2d2dd843d49d434dad894ebdc0ed5ce87c449b
Andrews Medina <andrewsmedina@gmail.com>
2012-05-08 22:09:29 -0300

    X and O player type

--- tictactoe.go
+++ tictactoe.go
@@ -7,3 +7,9 @@ type Board struct {
 func NewBoard() *Board {
 	return &Board{board: [9]uint8{}}
 }
+
+type Player struct {
+	t uint8
+}
+
+var X, O = uint8(1), uint8(2)
--- tictactoe_test.go
+++ tictactoe_test.go
@@ -16,3 +16,11 @@ func (s *S) TestNewBoard(c *C) {
 	expected := [9]uint8{}
 	c.Assert(board.board, DeepEquals, expected)
 }
+
+func (s *S) TestX(c *C) {
+	c.Assert(X, Equals, uint8(1))
+}
+
+func (s *S) TestO(c *C) {
+	c.Assert(O, Equals, uint8(2))
+}
